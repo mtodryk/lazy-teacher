aa862c0362916daaa4f61de41cb03643b9f50b25
Maksim Todryk <161019652+mtodryk@users.noreply.github.com>
2026-03-25 00:44:08 +0300

    Clean up urls.py by removing unused imports
    Removed unused imports and commented-out code related to static files.

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -1,8 +1,5 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -13,7 +10,7 @@ urlpatterns = [
     path("api/documents/", include("documents.urls")),
     path("api/users/", include("users.urls")),
     path("api/tests/", include("tests.urls")),
-    
+
     # Swagger / OpenAPI
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
@@ -22,6 +19,3 @@ urlpatterns = [
         name="swagger-ui",
     ),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
